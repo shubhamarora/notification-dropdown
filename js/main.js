9932f8b17ded725ba027c3fd3e5f302363009dc9
shubhamarora <shubham.3293@gmail.com>
2015-10-08 01:30:11 +0530

    ajax polling

--- js/main.js
+++ js/main.js
@@ -3,16 +3,31 @@
  */
 
 $(document).ready(function () {
+
+    // send request for new notifications after random intervals
+//    var ran = Math.floor((Math.random() * 1500) + 20000);
+//    var t = setTimeout(getNotifications,25000);
+
     $(".icon-bell,.noti-number").click(function () {
         if($(".noti-dropdown").css('display') == "none") {
-            $(".noti-dropdown")
-                .show()
+            $(".noti-dropdown").show()
                 .animate({top:'200%',opacity:1},800);
         }
         else {
-            $(".noti-dropdown")
-                .animate({"top":"250%","opacity":"0.1"},800)
-                .hide();
+            $(".noti-dropdown").hide()
+                .css({"top":"250%","opacity":"0.1"});
+            $(".noti-number, .noti-title-number").hide();
         }
     });
+
+    (function getNotifications() {
+        $.ajax({
+            url: "",
+            success: function (response) {
+                $(".noti-number, .noti-title-number").show();
+
+            }
+        });
+        setTimeout(getNotifications,Math.floor((Math.random() * 1500) + 5000));
+    })();
 });

3be9fa802185ec963ac1793e96a77949954f6db5
Shubham Arora <shubham.arora@comprotechnologies.com>
2015-10-08 13:49:35 +0530

    imporvments

--- js/main.js
+++ js/main.js
@@ -4,30 +4,62 @@
 
 $(document).ready(function () {
 
-    // send request for new notifications after random intervals
-//    var ran = Math.floor((Math.random() * 1500) + 20000);
-//    var t = setTimeout(getNotifications,25000);
+    $('html').on('click', function (event) {
+        if(!($(event.target).hasClass('icon-bell') || $(event.target).hasClass('noti-number'))) {
+            $(".noti-dropdown").hide()
+                .css({"top":"300%","opacity":"0.1"});;
+        }
+    });
 
     $(".icon-bell,.noti-number").click(function () {
         if($(".noti-dropdown").css('display') == "none") {
             $(".noti-dropdown").show()
-                .animate({top:'200%',opacity:1},800);
+                .animate({top:'250%',opacity:1},800);
+            $(".noti-number, .noti-title-number").hide();
         }
         else {
             $(".noti-dropdown").hide()
-                .css({"top":"250%","opacity":"0.1"});
+                .css({"top":"300%","opacity":"0.1"});
             $(".noti-number, .noti-title-number").hide();
         }
     });
 
     (function getNotifications() {
         $.ajax({
-            url: "",
+            url: "data.json",
             success: function (response) {
-                $(".noti-number, .noti-title-number").show();
 
+                if($(".noti-number").css('display') == 'none') {
+                    $(".noti-dropdown > li").slice(1).remove();
+                    $.each(response.data.items, function (i, item) {
+                        var randomNum = Math.floor((Math.random() * 1000) + 1);
+                        $(".noti-dropdown").append(
+                                '<li>'+
+                                '<a href=\"' + item.link + '\">'+
+                                '<div class=\"noti-item-photo\"><span style=\"background:url(' + item.photo +')\"></span></div>'+
+                                '<div class=\"noti-item-content\">'+ item.content + ' - ' + randomNum +'</div>'+
+                                '</a>'+
+                                '</li>').animate({"background-color":"#E9EFF2"},500);
+                    });
+                    $(".noti-number, .noti-title-number").empty().append(response.data.count).show();
+                }
+                else {
+                    $.each(response.data.items, function (i, item) {
+                        var randomNum = Math.floor((Math.random() * 1000) + 1);
+                        $("ul.noti-dropdown > li:first").after(
+                                '<li>'+
+                                '<a href=\"' + item.link + '\">'+
+                                '<div class=\"noti-item-photo\"><span style=\"background:url(' + item.photo +')\"></span></div>'+
+                                '<div class=\"noti-item-content\">'+ item.content + ' - ' + randomNum +'</div>'+
+                                '</a>'+
+                                '</li>');
+                    });
+                    $(".noti-number, .noti-title-number").text(parseInt($(".noti-number").text()) + response.data.count);
+                }
             }
         });
-        setTimeout(getNotifications,Math.floor((Math.random() * 1500) + 5000));
+
+        // send request for new notifications after random intervals
+        setTimeout(getNotifications,Math.floor((Math.random() * 1500) + 10000));
     })();
 });

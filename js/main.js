a2b35514f7c5831f5a26c3c10bde237717cfd580
shubhamarora <shubham.3293@gmail.com>
2015-10-08 22:40:00 +0530

    improvements

--- js/main.js
+++ js/main.js
@@ -5,9 +5,10 @@
 $(document).ready(function () {
 
     $('html').on('click', function (event) {
-        if(!($(event.target).hasClass('icon-bell') || $(event.target).hasClass('noti-number') || $(event.target).is('.noti-dropdown >li:first'))) {
+        if(!($(event.target).hasClass('icon-bell') || $(event.target).hasClass('noti-number'))) {
             $(".noti-dropdown").hide()
-                .css({"top":"300%","opacity":"0.1"});;
+                .css({"top":"300%","opacity":"0.1"});
+            $(".noti-dropdown > li").removeClass("new");
         }
     });
 
@@ -15,12 +16,13 @@ $(document).ready(function () {
         if($(".noti-dropdown").css('display') == "none") {
             $(".noti-dropdown").show()
                 .animate({top:'250%',opacity:1},800);
-            $(".noti-number, .noti-title-number").hide();
+            $(".noti-number, .noti-title-number").hide().text("0");
         }
         else {
             $(".noti-dropdown").hide()
                 .css({"top":"300%","opacity":"0.1"});
-            $(".noti-number, .noti-title-number").hide();
+            $(".noti-number, .noti-title-number").hide().text("0");
+            $(".noti-dropdown > li").removeClass("new");
         }
     });
 
@@ -28,34 +30,27 @@ $(document).ready(function () {
         $.ajax({
             url: "data.json",
             success: function (response) {
+                $.each(response.data.items, function (i, item) {
 
-                if($(".noti-number").css('display') == 'none') {
-                    $(".noti-dropdown > li").slice(1).remove();
-                    $.each(response.data.items, function (i, item) {
-                        var randomNum = Math.floor((Math.random() * 1000) + 1);
-                        $(".noti-dropdown").append(
-                                '<li>'+
-                                '<a href=\"' + item.link + '\">'+
-                                '<div class=\"noti-item-photo\"><span style=\"background:url(' + item.photo +')\"></span></div>'+
-                                '<div class=\"noti-item-content\">'+ item.content + ' - ' + randomNum +'</div>'+
-                                '</a>'+
-                                '</li>');
-                    });
-                    $(".noti-number, .noti-title-number").empty().append(response.data.count).show();
-                }
-                else {
-                    $.each(response.data.items, function (i, item) {
-                        var randomNum = Math.floor((Math.random() * 1000) + 1);
-                        $("ul.noti-dropdown > li:first").after(
-                                '<li>'+
-                                '<a href=\"' + item.link + '\">'+
-                                '<div class=\"noti-item-photo\"><span style=\"background:url(' + item.photo +')\"></span></div>'+
-                                '<div class=\"noti-item-content\">'+ item.content + ' - ' + randomNum +'</div>'+
-                                '</a>'+
-                                '</li>');
-                    });
-                    $(".noti-number, .noti-title-number").text(parseInt($(".noti-number").text()) + response.data.count);
-                }
+                    // used to append a random number in notification content
+                    // to make sure new notification is pushed in the list
+                    // for testing purpose, not to be used in production
+                    var randomNum = Math.floor((Math.random() * 1000) + 1);
+
+                    // add new notification in the drop - prepend in the list
+                    $("ul.noti-dropdown > li:first").after(
+                            '<li class=\"new\">'+
+                            '<a href=\"' + item.link + '\">'+
+                            '<div class=\"noti-item-photo\"><span style=\"background:url(' + item.photo +')\"></span></div>'+
+                            '<div class=\"noti-item-content\">'+ item.content + ' - ' + randomNum +'</div>'+
+                            '</a>'+
+                            '</li>');
+
+                    // animate new notification background color
+                    $(".noti-dropdown > li").animate({"opacity": "1"},2000);
+                });
+                    // update new notification count
+                    $(".noti-number, .noti-title-number").text(parseInt($(".noti-number").text()) + response.data.count).show();
             }
         });
 
